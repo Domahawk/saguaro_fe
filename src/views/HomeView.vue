a0fd22c8b60f97b49def20865f71acf767d13d92
Domahawk <d.biscan.stu@gmail.com>
2025-07-17 16:35:50 +0200

    Add User Home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
+import { useUserStore } from "@/stores/userStore.ts";
+import type { User } from "@/types/user.ts";
+import { onMounted, ref, type Ref } from "vue";
+
+const userStore = useUserStore();
+
+const user: Ref<User | undefined> = ref<User>();
+
+onMounted(() => {
+  user.value = userStore.user
+})
 
 </script>
 
 <template>
-<h1>This is a registered non admin user Home View</h1>
+<h1>Hello, {{ user?.username }}</h1>
+  <h2>Your information</h2>
+  <p>Username: {{ user?.username }}</p>
+  <p>Email: {{ user?.email }}</p>
+  <p>Admin privileges: {{ user?.admin }}</p>
 </template>
 
 <style scoped>
